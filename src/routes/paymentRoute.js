c1c81007460dae5575ad94e8188e5a008c88cd8c
pukpuk555 <jester2540@gmail.com>
2024-07-12 10:36:05 +0700

    change paymentcontroller path

--- src/routes/paymentRoute.js
+++ src/routes/paymentRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   authenticateMiddleware,
 } = require("../middleware/authenticateMiddleware");
-const paymentController = require("../controllers/PaymentController");
+const paymentController = require("../controllers/paymentController");
 
 const router = express.Router();
 
