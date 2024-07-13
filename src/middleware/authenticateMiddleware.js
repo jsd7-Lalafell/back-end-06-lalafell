574d18398eab713b3e74ce928d12076d55bfa588
Satapirom Khawatthu. <Satapirom.12@gmail.com>
2024-07-13 21:35:23 +0700

    update cart

--- src/middleware/authenticateMiddleware.js
+++ src/middleware/authenticateMiddleware.js
@@ -4,6 +4,9 @@ const User = require("../models/user.model");
 const authenticateMiddleware = async (req, res, next) => {
   try {
     const authHeader = req.headers["authorization"];
+    console.log(authHeader)
+
+
     const token = authHeader && authHeader.split(" ")[1];
     if (!token)
       return res.status(401).json({ error: true, message: "ไม่ได้รับอนุญาต" });
@@ -12,6 +15,7 @@ const authenticateMiddleware = async (req, res, next) => {
     const user = await User.findById(decoded.user);
     if (!user)
       return res.status(401).json({ error: true, message: "ไม่พบผู้ใช้" });
+    console.log(user)
 
     req.user = user;
     next();
